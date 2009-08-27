b4b753b100bbf9fcd247f9851920c2144bc5d904
rootuser <saurabh@noisybrain.net>
2009-08-27 16:04:20 +0530

    Added Comatose For Content Management of the Pages Added Restful_Authentication for Authentication

---
+++ db/schema.rb
@@ -0,0 +1,59 @@
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
+# please use the migrations feature of Active Record to incrementally modify your database, and
+# then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your database schema. If you need
+# to create the application database on another system, you should be using db:schema:load, not running
+# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20090827065754) do
+
+  create_table "comatose_page_versions", :force => true do |t|
+    t.integer  "comatose_page_id"
+    t.integer  "version"
+    t.integer  "parent_id"
+    t.text     "full_path"
+    t.string   "title"
+    t.string   "slug"
+    t.string   "keywords"
+    t.text     "body"
+    t.string   "filter_type",      :limit => 25, :default => "Textile"
+    t.string   "author"
+    t.integer  "position",                       :default => 0
+    t.datetime "updated_on"
+    t.datetime "created_on"
+  end
+
+  create_table "comatose_pages", :force => true do |t|
+    t.integer  "parent_id"
+    t.text     "full_path"
+    t.string   "title"
+    t.string   "slug"
+    t.string   "keywords"
+    t.text     "body"
+    t.string   "filter_type", :limit => 25, :default => "Textile"
+    t.string   "author"
+    t.integer  "position",                  :default => 0
+    t.integer  "version"
+    t.datetime "updated_on"
+    t.datetime "created_on"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
+
+end
